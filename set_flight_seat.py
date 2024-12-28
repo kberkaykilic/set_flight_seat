6a54f4402e4722c69a9b8a385b7a7693c8c52c7d
Berkay Kılıç <kberkaykilic@icloud.com>
2024-12-29 01:06:16 +0300

    pegasus seats updated!

--- set_flight_seat.py
+++ set_flight_seat.py
@@ -26,6 +26,12 @@ def is_thy_full():
             return False
     return True
 
+def is_pgs_full():
+    for i in range(10):
+        if pegasus_seats[i] == False:
+            return False
+    return True
+
 
 def thy():
     if is_thy_full():
@@ -35,14 +41,15 @@ def thy():
           "2-Ekonomi\n")
     secim_2 = int(input("Seçiminiz: "))
 
+    # Business THY
     if secim_2 == 1:
         if is_thy_business_full():
             print("Business full")
             return
-        print(f"THY business seats= ")
+        print(f"Available THY business seats= \n")
         for i in range(5):
             if thy_seats[i] == False:
-                print(f"{i+1}", end=" ")
+                print(f"{i+1}", end="\n")
         print()
         koltuk = int(input("Koltuk(1-5 arası giriniz.): "))
         if 1 <= koltuk <= 5:
@@ -50,14 +57,15 @@ def thy():
             print(f"{koltuk} numaralı koltuk size rezerveedildi, iyi uçuşlar!")
         else:
             print("Gecersiz koltuk secimi yaptiniz.")
+    # Ekonomi THY
     elif secim_2 == 2:
         if is_thy_ekonomi_full():
-            print("ekonomi full")
+            print("Ekonomi full\n")
             return
-        print(f"THY ekonomi seats= ")
+        print(f"THY ekonomi seats= \n")
         for i in range(5, 10):
             if thy_seats[i] == False:
-                print(f"{i + 1}", end=" ")
+                print(f"{i + 1}", end="\n")
         print()
         r_koltuk = random.randrange(6, 11)
         while True:
@@ -69,14 +77,29 @@ def thy():
                 return
 
 def pegasus():
-    pass
+    if is_pgs_full():
+        print("Ekonomi full\n")
+        return
+    print(f"Available Pegasus ekonomi seats= \n")
+    for i in range(10):
+        if pegasus_seats[i] == False:
+            print(f"{i+1}", end="-")
+    print()
+    p_koltuk = random.randrange(1, 11)
+    while True:
+        if pegasus_seats[p_koltuk - 1] == True:
+            p_koltuk = random.randrange(1, 11)
+        else:
+            pegasus_seats[p_koltuk - 1] = True
+            print(f"{p_koltuk} numaralı koltuk size rezerveedildi, iyi uçuşlar!")
+            return
 
 while True:
     print_menu()
     secim = int(input("Seçiminiz: "))
 
     if secim==1:
-        print("THY Sistemine hoşgeldiniz")
+        print("THY Sistemine hoşgeldiniz, hangi klasmanı tercih edersiniz?\n")
         thy()
     elif secim==2:
         print("Pegasus sistemine hoşgeldiniz")
